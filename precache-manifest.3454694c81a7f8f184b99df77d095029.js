dcfe758e01a7a4e7f7d8b5f241ceb62b8c2d70e1
rodineiteixeira <rodinei.teixeira@atitude.com.br>
2019-07-15 18:08:33 -0300

    Updates

---
+++ precache-manifest.3454694c81a7f8f184b99df77d095029.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cc28238d11b0678eb21265a11429f6e",
+    "url": "/cc-gh-pages/index.html"
+  },
+  {
+    "revision": "84604a4632c3e5b496d6",
+    "url": "/cc-gh-pages/static/css/main.f9b18540.chunk.css"
+  },
+  {
+    "revision": "7c7c0e6b98ec24a95992",
+    "url": "/cc-gh-pages/static/js/2.550d9ff0.chunk.js"
+  },
+  {
+    "revision": "84604a4632c3e5b496d6",
+    "url": "/cc-gh-pages/static/js/main.f71d30c0.chunk.js"
+  },
+  {
+    "revision": "fbfc7e33a8dd60133760",
+    "url": "/cc-gh-pages/static/js/runtime~main.c8c0c772.js"
+  }
+]);
