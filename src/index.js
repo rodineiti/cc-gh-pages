ae95e3114afce29610b1aee6fc042fcb2875fec6
rodineiteixeira <rodinei.teixeira@atitude.com.br>
2019-07-15 18:13:31 -0300

    .

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import { InMemoryCache } from 'apollo-cache-inmemory';
 
 
 const httpLink = createHttpLink({
-    uri: ''
+    uri: 'https://black-mage-devel--brunolcarli.repl.co/graphql'
 });
 
 const client = new ApolloClient({
